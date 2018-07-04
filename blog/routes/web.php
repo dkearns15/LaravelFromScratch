9ca52c87df5c68e32d6e62325042e3dd3b12be68
dkearns15 <dkearns@hotmail.com.au>
2018-07-04 15:50:45 +0930

    Route Model Binding

--- blog/routes/web.php
+++ blog/routes/web.php
@@ -11,29 +11,5 @@
 |
 */
 
-use App\Task;
-
-Route::get('/', function () {
-
-    $tasks = DB::table('tasks')->latest()->get();
-
-    return view('welcome', compact('tasks'));
-});
-
-Route::get('/tasks', function () {
-
-    $tasks = Task::all();
-
-    return view('tasks.index', compact('tasks'));
-});
-
-Route::get('/tasks/{task}', function ($id) {
-
-    $task = Task::find($id);
-
-    return view('tasks.show', compact('task'));
-});
-
-Route::get('/about', function () {
-    return view('about');
-});
+Route::get('/tasks', 'TasksController@index');
+Route::get('/tasks/{task}', 'TasksController@show');
