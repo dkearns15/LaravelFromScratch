3c32677b3930bbd45b0dc587f4f68a697fb9492f
dkearns15 <dkearns@hotmail.com.au>
2018-07-04 15:22:38 +0930

    Eloquent 101

---
+++ blog/app/Task.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Task extends Model
+{
+    public function scopeIncomplete($query, $val){
+      return $query->where('completed', 0);
+    }
+}
--- blog/routes/web.php
+++ blog/routes/web.php
@@ -11,6 +11,8 @@
 |
 */
 
+use App\Task;
+
 Route::get('/', function () {
 
     $tasks = DB::table('tasks')->latest()->get();
@@ -20,14 +22,14 @@ Route::get('/', function () {
 
 Route::get('/tasks', function () {
 
-    $tasks = DB::table('tasks')->latest()->get();
+    $tasks = Task::all();
 
     return view('tasks.index', compact('tasks'));
 });
 
-Route::get('/tasks/{id}', function ($id) {
+Route::get('/tasks/{task}', function ($id) {
 
-    $task = DB::table('tasks')->find($id);
+    $task = Task::find($id);
 
     return view('tasks.show', compact('task'));
 });
