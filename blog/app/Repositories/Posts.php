13cfd1a0484a3328bb1f73bfb4333bcc2a9183fb
dkearns15 <dkearns@hotmail.com.au>
2018-07-09 16:45:28 +0930

    DI, Auto-Resolution, and Repositories

---
+++ blog/app/Repositories/Posts.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Repositories;
+
+use App\Post;
+
+class Posts{
+
+    public function all(){
+        return Post::all();
+    }
+
+    public function find(){
+
+
+    }
+
+
+}
+
+
+ ?>
